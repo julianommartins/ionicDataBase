045c3d9ae83eb97461f3ece2f1c4cb633a0abc22
Juliano Martins <juliano.jmm@gmail.com>
2015-08-21 18:54:51 -0300

    CRUD and List completed

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -22,28 +22,67 @@ var starter = angular.module('starter', ['ionic', 'ngCordova'])
 starter.controller('DBController', function($scope, $cordovaSQLite) {
   
     $scope.resultado = "";
+    $scope.peopleList = [];
     
     $scope.insert = function(firstname, lastname) {
+        $scope.peopleList = [];
         var query = "insert into pessoas (firstname, lastname) values (?,?)";
         $cordovaSQLite.execute(db,query,[firstname,lastname]).then(function(result) {
             //$scope.resultado.push({mensagem: "Insert Ok"});
-            $scope.resultado = "Insert Ok";
+            $scope.resultado = "Insert OK.";
         }, function(error){
-            $scope.resultado = "Insert FAIL";
+            $scope.resultado = "Insert FAIL!";
         });
     }
     
     $scope.select = function(lastname){
+        $scope.peopleList = [];
         var query = "select firstname, lastname from pessoas where lastname = ?";
         $cordovaSQLite.execute(db,query,[lastname]).then(function(result) {
             if(result.rows.length > 0){
                 $scope.resultado = result.rows.item(0).firstname + " found with this last name.";
             } else {
-                $scope.resultado = "Not found";
+                $scope.resultado = "Last Name Not Found!";
             }
-            
         }, function(error){
             console.log(error);
+        });
+    }
+    
+    $scope.selectAll = function(){
+        var query = "select firstname, lastname from pessoas";
+        $cordovaSQLite.execute(db,query,[]).then(function(result) {
+            if(result.rows.length > 0){
+                for(var i = 0; i < result.rows.length; i++) {
+                    $scope.peopleList.push({firstname: result.rows.item(i).firstname, lastname: result.rows.item(i).lastname});
+                }
+                $scope.resultado = result.rows.length + " rows found.";
+            } else {
+                $scope.resultado = "0 rows found!";
+                $scope.peopleList = [];
+            }
+        }, function(error){
+            console.log(error);
+        });
+    }
+    
+    $scope.delete = function(lastname) {
+        $scope.peopleList = [];
+        var query = "delete from pessoas where lastname = ?";
+        $cordovaSQLite.execute(db,query,[lastname]).then(function(result) {
+            $scope.resultado = "Delete Ok.";
+        }, function(error){
+            $scope.resultado = "Delete FAIL!";
+        });
+    }
+    
+     $scope.update = function(firstname, lastname) {
+        $scope.peopleList = [];
+        var query = "update pessoas set firstname = ? where lastname = ?";
+        $cordovaSQLite.execute(db,query,[firstname,lastname]).then(function(result) {
+            $scope.resultado = "Update OK.";
+        }, function(error){
+            $scope.resultado = "Update FAIL!";
         });
     }
     
